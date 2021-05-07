ab9a93e4b1db0c59c48cc1a8abfef70617cf4a9b
Ubuntu <ubuntu@ip-172-31-45-144.us-east-2.compute.internal>
2021-05-07 19:26:21 +0000

    Added proofs script to provide proof on oracle_proof query

--- backend/index_proof.js
+++ backend/index_proof.js
@@ -1,24 +1,23 @@
 // For encrypted queries
 const requireESM = require('esm')(module);
 const fs = require('fs');
+const web3 = require('web3');
+const pgsg = require('./pagesigner/webextension/content/pgsg-node/pgsg-node');
+var path_ = require('path')
 const { Universal, Node, MemoryAccount, ContractACI } = require('@aeternity/aepp-sdk');
 const { decodeEvents, SOPHIA_TYPES } = requireESM('@aeternity/aepp-sdk/es/contract/aci/transformation');
-// query example: https://poloniex.com/public?command=returnTicker,BTC_BTS,id
+// query example: https://api.pro.coinbase.com/products/ETH-USD/ticker,price
 // import { parseBigNumber, asBigNumber, isBigNumber, ceil } from '@aeternity/aepp-sdk/es/utils/bignumber'
 const OracleContractCode = fs.readFileSync(__dirname + '/../contracts/OracleConnector.aes', 'utf-8');
 // oracle_plain
-const contract_address = "ct_2uEWFtsqEhErztjthHrt1UD5QPLAfnpWjDbYUN6FAT5aVZGyUd"
+const contract_address = "ct_2m6mJzrPgrfynFcxhDx33iZRqRjkke1unfRrhjBBfyEbpQZkiK"
 var blake2b = require('blake2b')
 var axios = require('axios')
-var url = "https://sdk-testnet.aepps.com"
+var url = "https://testnet.aeternity.io/"
 var processedIndex = 0
-var Compilerurl = "https://sdk-testnet.aepps.com"
+var Compilerurl = "https://testnet.aeternity.io/"
 const BigNumber = require('bignumber.js');
 const conf = require('./conf.json')
-
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr(process.env.SECRET_KEY);
-
 
 const keyPair = {
   "publicKey": "ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU",
@@ -74,10 +73,8 @@ async function StartListening () {
         let _getQueryQuestion = getQueryQuestion.decodedResult
         console.log("getQueryQuestion: " + _getQueryQuestion)
 
-        let _decrypted_query_question = cryptr.decrypt(encryptedString);
-        let result = await getResult(_decrypted_query_question)
-        console.log("result " + result)
-
+        let result = await getResultWithTLSProof(_getQueryQuestion)
+        console.log("result:- " + result)
         let respondToQuery = await contract.methods.respond(_getQuery, result.toString())
         let _respondToQuery = respondToQuery.decodedResult
         console.log("respondToQuery: " + _respondToQuery)
@@ -93,11 +90,11 @@ async function StartListening () {
   // setInterval(getQueries, 10000);
 }
 // 
-async function getResult (_args) {
+
+async function getResultWithTLSProof (_args) {
   let args = _args.split(',')
-  var response = null
   try {
-    response = await axios.get(args[0])
+    await axios.get(args[0])
   }
   catch (err) {
     console.log("ERR ========")
@@ -105,11 +102,50 @@ async function getResult (_args) {
     console.log("ERR END ========")
     return "ERR:URL"
   }
+  console.log("args: " + args)
+  const myURL = new URL(args[0]);
 
-  var result = response.data
-  for (let index = 1; index < args.length; index++) { // nested results
-    result = result[args[index]]
+  const host = myURL.host
+  const path = myURL.pathname
+
+  console.log("WITH TLS Notary Proof")
+  var headers = `GET ${path} HTTP/1.1\naccept: application/json\nHost: ${host}\n\n`
+  console.log(headers)
+  var response_dir = await pgsg.main(["notarize", host, headers])
+  const dirpath = path_.join("./pagesigner/webextension/content/pgsg-node", response_dir)
+
+  var targetFiles = null;
+  console.log("------------------------")
+  console.log("dirpath: " + dirpath)
+  console.log("------------------------")
+  var targetFiles = fs.readdirSync(dirpath)
+  const pgsgFile = targetFiles.filter(el => path_.extname(el) === '.pgsg')
+
+  var response_verified_dir = await pgsg.main(["verify", response_dir + `/` + pgsgFile])
+  console.log("response_verified_dir: " + response_verified_dir)
+  const dirpath_verified = path_.join("./pagesigner/webextension/content/pgsg-node", response_verified_dir)
+  console.log("dirpath_verified: " + dirpath_verified)
+  var clear_text = fs.readFileSync(path_.join(dirpath_verified, "cleartext")).toString()
+  var pgsg_proof = fs.readFileSync(dirpath_verified + "/" + pgsgFile).toString()
+
+  var result_array = clear_text.split("\r\n\r\n")
+  console.log(result_array)
+  var result_json = null
+  try {
+    result_json = JSON.parse(result_array[1].toString())
   }
-  console.log("result: " + result)
-  return result
+  catch (err) {
+    console.log("ERR ========")
+    console.log(err.message)
+    console.log("ERR END ========")
+  }
+  for (let index = 1; index < args.length; index++) { // nested results
+    result_json = result_json[args[index]]
+  }
+  console.log("result: " + result_json)
+  console.log("\n\nproof: " + pgsg_proof)
+  var proof_in_bytes = web3.utils.toHex(pgsg_proof)
+  console.log("\n\nproof in Bytes: " + proof_in_bytes)
+  return `${result_json},${proof_in_bytes}`
+
 }
